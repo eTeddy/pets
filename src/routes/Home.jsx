ffd5e6b20fd020489e5ae6c7a5a9e697cbd97a5b
eTeddy <tpetw886@gmail.com>
2023-05-05 14:49:28 -0500

    Add Home Page

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -1,9 +1,23 @@
 function Home() {
+  const currentYear = new Date().getFullYear();
+
   return (
-    <div className="home">
-      <h1>Home</h1>
-    </div>
-  );
+    <main>
+      <h1>Inspired By</h1>
+      <p>-----------------------------------</p>
+      <p>Brian Design<br />React Sidebar Navigation Menu<br />using React Router v6.4<br />Beginner Tutorial</p>
+      <iframe width="300" height="auto" src="https://www.youtube.com/embed/zQBd3hNXJgI" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+      <p>-----------------------------------</p>
+      <p>PedroTech<br />React Router V6.4+ Tutorial<br />New Syntax, useLoaderData...</p>
+      <iframe width="300" height="auto" src="https://www.youtube.com/embed/z0vaVoxMoSA" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+      <p>-----------------------------------</p>
+      <p>The Net Ninja<br />React Router in Depth</p>
+      <iframe width="300" height="auto" src="https://www.youtube.com/embed/OMQ2QARHPo0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+      <p>-----------------------------------</p>
+      <h1>🧡</h1>
+      <p>@ May 2023</p>
+    </main>
+  )
 }
 
 export default Home;
