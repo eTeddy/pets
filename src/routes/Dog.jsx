c129c9d0190555e4f89c810a882f9560fecdd2a8
eTeddy <tpetw886@gmail.com>
2023-05-05 14:11:06 -0500

    Add Dog and Fox

--- src/routes/Dog.jsx
+++ src/routes/Dog.jsx
@@ -1,3 +1,46 @@
+import { 
+  useLoaderData, 
+  useNavigation,
+} from "react-router-dom"
+import { useEffect, useState } from "react";
+
+export default function Dog() {
+  const [ dogUrl, setDogUrl ] = useState(useLoaderData());
+  const navigation = useNavigation();
+
+  const handleNewDog = async () => {
+    const res = await fetch("https://dog.ceo/api/breeds/image/random");
+    const dog = await res.json();
+    setDogUrl(dog.message);
+  };
+
+  useEffect(() => {
+    handleNewDog();
+  }, []);
+    
+  if ( navigation.state === "loading") { return <h1>Loading...</h1> }
+  
+  return (
+    <main>
+      <button onClick={handleNewDog}>Click 🐶 Me</button> 
+      <img src={dogUrl} alt="dog"/>  
+      <div className="a">
+        <a href="https://dog.ceo/" target="_blank">DOG CEO</a>
+      </div>       
+    </main>
+  ) 
+}
+
+export const dataLoader = async () => {
+  const res = await fetch("https://dog.ceo/api/breeds/image/random");
+  const dog = await res.json();
+
+  return dog.message;
+}
+
+
+/*
+
 export default function Dog() {
   return (
     <div className="dog">
@@ -5,3 +48,4 @@ export default function Dog() {
     </div>
   )
 }
+*/
--- src/routes/Fox.jsx
+++ src/routes/Fox.jsx
@@ -1,7 +1,16 @@
+import { useState } from "react";
+
 export default function Fox() {
+	const [ num, setNum ] = useState(Math.floor(Math.random() * 123) + 1);
+	const newnum = () => { setNum(Math.floor(Math.random() * 123) + 1) }
+  
   return (
-    <div className="fox">
-      🦊
-    </div>
+    <main>
+      <button onClick={newnum}>New 🦊 Fox</button>					
+			<img src={`https://randomfox.ca/images/${num}.jpg`} alt="fox" />        
+      <div className="a">
+        <a href="https://randomfox.ca/" target="_blank">Random Fox</a>
+      </div>    
+    </main>
   )
 }
