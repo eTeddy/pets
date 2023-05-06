4163b406fdfc4d5780a73c76f9cfce3a2be922ca
eTeddy <tpetw886@gmail.com>
2023-05-05 20:36:16 -0500

    Eye

--- src/routes/Eye.jsx
+++ src/routes/Eye.jsx
@@ -1,7 +1,9 @@
 function Eye() {
   return (
     <main>
+      <p>---------------</p>
       <h1>Namaste!</h1>      
+      <p>---------------</p>
       <span className="eye">🪬</span>      
     </main>
   );
