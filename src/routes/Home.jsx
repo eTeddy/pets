4abf16b627416e1b3ddbf6c5c4a7089178de19f7
eTeddy <tpetw886@gmail.com>
2023-05-06 20:53:59 -0500

    Fix Home

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -15,9 +15,13 @@ function Home() {
       <iframe width="300" height="auto" src="https://www.youtube.com/embed/OMQ2QARHPo0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
       <p>-----------------------------------</p>
       <h1>Powered By</h1>
-      <p>HTML, CSS, JavaScript, JSX, APIs, Vite, React & 🧡</p>
-      <p>-----------------------------------</p>
-      <p>Minimalist @ May 2023</p>
+      <p>
+        HTML, CSS, JavaScript, JSX, 
+        <br />
+        APIs, Vite & React!
+      </p>
+      <p>Minimalist 🧡 May 2023</p>
+      <p>-----------------------------------</p>      
       <a href="https://www.worldflagcounter.com/details/iuP"><img src="https://www.worldflagcounter.com/iuP/" alt="Flag Counter"></a>
     </main>
   )
