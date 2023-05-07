b79ab61fdb1cf800cdca83f045b77288f629ab08
eTeddy <tpetw886@gmail.com>
2023-05-06 20:38:33 -0500

    add APIs

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -15,7 +15,7 @@ function Home() {
       <iframe width="300" height="auto" src="https://www.youtube.com/embed/OMQ2QARHPo0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
       <p>-----------------------------------</p>
       <h1>Powered By</h1>
-      <p>HTML, CSS, JavaScript, JSX, Vite, React & 🧡</p>
+      <p>HTML, CSS, JavaScript, JSX, APIs, Vite, React & 🧡</p>
       <p>-----------------------------------</p>
       <p>Minimalist @ May 2023</p>
       <a href="https://www.worldflagcounter.com/details/iuN">
