c79e4a9d06be1cdf8dc775694a393833148ae237
eTeddy <tpetw886@gmail.com>
2023-05-06 21:59:04 -0500

    Fix p line height

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -6,13 +6,13 @@ function Home() {
       <h1>Inspired By</h1>
       <p>--------------------------------</p>
       <p>Brian Design<br />React Sidebar Navigation Menu<br />using React Router v6.4<br />Beginner Tutorial</p>
-      <iframe width="300" height="auto" src="https://www.youtube.com/embed/zQBd3hNXJgI" title="YouTube video player" frameborder="1" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+      <iframe width="280" height="auto" src="https://www.youtube.com/embed/zQBd3hNXJgI" title="YouTube video player" frameborder="1" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
       <p>--------------------------------</p>
       <p>PedroTech<br />React Router V6.4+ Tutorial<br />New Syntax, useLoaderData...</p>
-      <iframe width="300" height="auto" src="https://www.youtube.com/embed/z0vaVoxMoSA" title="YouTube video player" frameborder="1" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+      <iframe width="280" height="auto" src="https://www.youtube.com/embed/z0vaVoxMoSA" title="YouTube video player" frameborder="1" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
       <p>--------------------------------</p>
       <p>The Net Ninja<br />React Router in Depth</p>
-      <iframe width="300" height="auto" src="https://www.youtube.com/embed/OMQ2QARHPo0" title="YouTube video player" frameborder="1" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+      <iframe width="280" height="auto" src="https://www.youtube.com/embed/OMQ2QARHPo0" title="YouTube video player" frameborder="1" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
       <p>--------------------------------</p>
       <h1>Powered By</h1>
       <p>
@@ -20,10 +20,10 @@ function Home() {
         <br />
         APIs, Vite & React!
       </p>
-      <p>--------------------------------</p>
-      <p>Minimalist 🧡 May 2023</p>
       <p>--------------------------------</p>    
       <a href="https://www.worldflagcounter.com/details/iuP"><img src="https://www.worldflagcounter.com/iuP/" alt="Flag Counter" /></a>
+      <p>--------------------------------</p>
+      <p>Minimalist 🧡 May 2023</p>   
     </main>
   )
 }
